75156faabb045bcdead5dce2078799b5b2258684
Dubernet <thoms.dubernet@gmail.com>
2020-01-07 22:19:27 +0100

    ajout de la home

--- index.php
+++ index.php
@@ -1,6 +1,160 @@
 <?php get_header();?>
-Page d'acceuil
-<!-- content -->
+<!-- HOMEPAGE -->
+<!------------------------------------->
+<!-- Vidéo présentation - Section 0 -->
+<!------------------------------------->
+<div class="hero-block" style="position: relative;">
+    <div class="container">
+        <div class="row align-items-center">
+
+        <div class="col-6">
+            <h3>
+            Un mouvement citoyen au service de tous ceux qui s'engagent pour un futur souhaitable
+            </h3>
+        </div>
+        <div class="col-6">
+            <div class="logo">
+                <a href="#"><img src="images/logo.jpg" alt="image placeholder"></a>
+            </div>
+        </div>
+
+        </div>
+    </div>
+</div>
+
+
+<!-- Programmes d'action - Section 0.1 -->
+<!------------------------------------->
+<div class="container">
+    <div class="row">
+
+        <div class="col-3">
+            <h3 class="text-center">Service de<br>La Transition</h3>
+            <p class="text-center">
+            Pour permettre à chacun de vivre une expérience de Transition fondatrice dans un de nos lieux de bascule.
+            </p>
+            <button>En lire +</button>
+        </div>
+        <div class="col-3">
+            <h3 class="text-center">Coopération<br>& événements</h3>
+            <p class="text-center">
+            Pour faciliter le passage à l'action collectif via du suivi, de la mise en réseau et l'animation de temps fort.
+            </p>
+            <button>En lire +</button>
+        </div>
+        <div class="col-3">
+            <h3 class="text-center">Lobbying<br>& citoyen</h3>
+            <p class="text-center">
+            Pour nous donner les moyens d'interpeller nos proches, nos organisations et nos élus.
+            </p>
+            <button>En lire +</button>
+        </div>
+        <div class="col-3">
+            <h3 class="text-center">Soutien aux<br>Mobilisations</h3>
+            <p class="text-center">
+            Pour en faire de véritables leviers de Transition à travers un appui humain, logistique et de communication.
+            </p>
+            <button>En lire +</button>
+        </div>
+
+    </div>
+</div>
+
+<!-- Modal La Bascule - Section 0.2 -->
+<!------------------------------------->
+
+
+<!-- Moi dans tout ça - Section 0.3 -->
+<!------------------------------------->
+
+
+<!-- Une Paserelle - Section 0.4 -->
+<!------------------------------------->
+
+
+<!-- Events - Section 0.5 -->
+<!------------------------------------->
+<div class="container">
+    <div class="row">
+
+        <div class="col-3">
+        <div class="item">
+        <div class="block-agenda">
+            <div class="image">
+                <a href="#"><img src="images/img_1.jpg" alt="image placeholder"></a>
+            </div>
+            <div class="text">
+                <span class="picto-event"></span>
+                <p class="date">Le 26 Juillet 2019</p>
+                <p class="font-weight-bold">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </div>
+            <div class="hashtag">
+                <p>#blablabla</p>
+            </div>
+            <button>Voir l'événement</button>
+        </div>
+        </div>
+        </div>
+
+        <div class="col-3">
+        <div class="item">
+        <div class="block-agenda">
+            <div class="image">
+                <a href="#"><img src="images/img_1.jpg" alt="image placeholder"></a>
+            </div>
+            <div class="text">
+                <span class="picto-event"></span>
+                <p class="date">Le 26 Juillet 2019</p>
+                <p class="font-weight-bold">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </div>
+            <div class="hashtag">
+                <p>#blablabla</p>
+            </div>
+            <button>Voir l'événement</button>
+        </div>
+        </div>
+        </div>
+        
+        <div class="col-3">
+        <div class="item">
+        <div class="block-agenda">
+            <div class="image">
+                <a href="#"><img src="images/img_1.jpg" alt="image placeholder"></a>
+            </div>
+            <div class="text">
+                <span class="picto-event"></span>
+                <p class="date">Le 26 Juillet 2019</p>
+                <p class="font-weight-bold">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </div>
+            <div class="hashtag">
+                <p>#blablabla</p>
+            </div>
+            <button>Voir l'événement</button>
+        </div>
+        </div>
+        </div>
+
+        <div class="col-3">
+        <div class="item">
+        <div class="block-agenda">
+            <div class="image">
+                <a href="#"><img src="images/img_1.jpg" alt="image placeholder"></a>
+            </div>
+            <div class="text">
+                <span class="picto-event"></span>
+                <p class="date">Le 26 Juillet 2019</p>
+                <p class="font-weight-bold">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </div>
+            <div class="hashtag">
+                <p>#blablabla</p>
+            </div>
+            <button>Voir l'événement</button>
+        </div>
+        </div>
+        </div>
+
+    </div>
+</div>
 
 
 
